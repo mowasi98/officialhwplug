0f47c57a2241a5108f3d3bc0dc5981f398223396
mowasi98 <shahmohid611@gmail.com>
2026-02-22 15:05:50 +0000

    Clean up repository: Move website to Hwplug/website/ and remove proprietary bot code

--- hwplug-official-bot.js
+++
@@ -1,5 +0,0 @@
-// Homework Plug Official Bot
-// Automated homework completion bot for educational platforms
-// Platforms: Sparx Maths, Sparx Reader, Seneca, Educate
-
-// Bot initialized - awaiting configuration and instructions
